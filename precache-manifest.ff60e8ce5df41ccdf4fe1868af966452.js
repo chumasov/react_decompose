efca110916e3ad4483b269c9da34a14df69fb2de
Timur Chumasov <t.chumasov@gmail.com>
2021-05-18 13:14:11 +0300

    Updates

---
+++ precache-manifest.ff60e8ce5df41ccdf4fe1868af966452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d523d55d834fc39e3fe99cc4531182",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3815d5c13156fc578c03",
+    "url": "./static/css/main.1b687aeb.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3815d5c13156fc578c03",
+    "url": "./static/js/main.0413f324.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
